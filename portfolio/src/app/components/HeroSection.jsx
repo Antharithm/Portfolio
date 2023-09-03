df984b922b5024c0d2e414a1e936fefa3ab05ec7
Anthony <antharhythm@Anthonys-MacBook-Pro.local>
2023-09-03 19:45:40 -0400

    project start

---
+++ portfolio/src/app/components/HeroSection.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const HeroSection = () => {
+  return (
+    <section>
+      <h1 className="text-white">Hello, I&apos;m Anthony</h1>
+      <p>
+        I am a Full Stack Web Developer with experience in building web
+        applications using Solidity, JavaScript and Python.
+      </p>
+    </section>
+  );
+};
+
+export default HeroSection;
