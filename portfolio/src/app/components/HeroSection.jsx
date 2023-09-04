d92e33b77df930e9ffe8f167a4353e3d4c7a5c5a
Anthony <antharhythm@Anthonys-MacBook-Pro.local>
2023-09-04 17:47:04 -0400

    type animations

--- portfolio/src/app/components/HeroSection.jsx
+++ portfolio/src/app/components/HeroSection.jsx
@@ -1,32 +1,56 @@
+"use client";
 import React from "react";
 import Image from "next/image";
+// Components
+import { TypeAnimation } from "react-type-animation";
 
 const HeroSection = () => {
   return (
     <section>
       <div className="grid grid-cols-1 lg:grid-cols-12">
-        <div className="col-span-7 place-self-center">
-          <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">
-            Hello, I&apos;m Anthony
+        <div className="col-span-7 place-self-center text-center sm:text-left">
+          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
+            <span className="text-transparent bg-clip-text bg-gradient-to-r to-gray-400 from-gray-300">
+              Hello, I&apos;m{" "}
+            </span>
+            <br></br>
+            <TypeAnimation
+              sequence={[
+                "Anthony",
+                3000,
+                "Web Developer.",
+                1000,
+                "Blockchain Developer.",
+                1000,
+                "UI/UX Designer.",
+                1000,
+              ]}
+              wrapper="span"
+              speed={50}
+              style={{ fontSize: "1em", display: "inline-block", color: "#50fd9a" }}
+              repeat={Infinity}
+            />
           </h1>
-          <p className="text-[#ADB7BE] text-lg mb-6 lg:text-xl">
-            I am a Full Stack Web Developer with experience in building web
-            applications using Solidity, JavaScript and Python.
+          <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
+            I am a Full Stack Web Developer with experience in building web and mobile
+            applications using JavaScript, Solidity and Python.
           </p>
           <div>
-            <button className="px-6 py-3 rounded-full mr-4 bg-white hover:bg-slate-200 text-black">
+            <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-[#50fd9a] hover:bg-slate-600 text-white border border-gray-600">
               Hire Me
             </button>
-            <button className="px-6 py-3 rounded-full bg-transparent hover:bg-slate-700 text-white border border-white mt-3">
-              Download CV
+            <button className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-gray-300 hover:bg-gray-500 text-white mt-3">
+              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
+                Download CV
+              </span>
             </button>
           </div>
         </div>
         <div className="col-span-5 place-self-center mt-4 lg:mt-0">
           <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
             <Image
-              src="/images/circleProPic.png"
-              alt="sq-pro-pic.png"
+              src="/images/circleProPic2.png"
+              alt="/images/circleProPic.png"
               className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
               width={300}
               height={500}
